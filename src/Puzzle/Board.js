7f89d3634b9a1fee2278f233c4c04f89f4501a5c
Michael Garate <mpgarate@gmail.com>
2014-05-14 15:28:42 -0400

    add swap fn

--- src/Puzzle/Board.js
+++ src/Puzzle/Board.js
@@ -15,5 +15,38 @@ var Board = (function(){
     console.log(positions);
   }
 
+
+  // swap two positions in the grid
+  // each position is an array of x,y coords
+  Board.prototype.swap = function(pos1,pos2){
+    var tmp = positions[pos1[0]][pos1[1]];
+
+    positions[pos1[0]][pos1[1]] = positions[pos2[0]][pos2[1]];
+    
+    positions[pos2[0]][pos2[1]] = tmp;
+  }
+
+  // boolean check if table is in solved state. 
+  // Checks each position. 
+  Board.prototype.isSolved = function(){
+    var number = 0;
+
+    // loop through each row and column
+    for (var i = 0; i < positions.length; i++){
+      for (var j = 0; j < positions[i].length; j++){
+        if (positions[i][j] != number){
+          // number should always match value, except when the last square
+          // in this case, the value is null. 
+          if (i != positions.length -1 && j != positions[i].length -1){
+            return false;
+          }
+        }
+        number++;
+      }
+    }
+
+    return true;
+  }
+
   return Board;
 })();
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 var positions = [
-                  [0,1,2,3],
-                  [4,5,6,7],
+                  [0,6,2,3],
+                  [4,5,1,7],
                   [8, 9, 10,11],
                   [12,13,14,null]
                 ]
 
 var board = new Board(positions);
-board.printPositions();
+
 
 //PuzzleAI.solve(board);
-
